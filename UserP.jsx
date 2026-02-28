8f0533e46097ee4cfe84d195b1c5880115560be0
Amit <h4775990@gmail.com>
2026-02-28 12:45:00 +0530

    fixed

--- UserP.jsx
+++ UserP.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const UserP = () => {
-  return <>
-    <div>UserP</div>
-  </>
-}
+  return (
+    <>
+      <div>UserP</div>
+    </>
+  );
+};
 
 export default UserP;
